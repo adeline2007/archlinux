d4e2253fda3bde05cd27e4858313777b2fd373ae
Vsevolod <85790218+adeline2007@users.noreply.github.com>
2025-03-16 01:10:23 +0500

    Update start_fetch.py

--- start_fetch.py
+++ start_fetch.py
@@ -1,11 +1,13 @@
 import subprocess
-from random import randint
+import os
+from random import randint, choice
 import time
 
 # https://copypastas.ru/image/   website where you can do ascii art
 
 
 def make_random_text():
+    # Здесь можно в список добавлять текст и он случайно будет выбираться при запуске
     list_text = [
         "Crede firmiter et pecca fortiter )",
         "Хорошего дня тебе солнце!)",
@@ -18,6 +20,7 @@ def make_random_text():
     return list_text[randint(0, len(list_text)-1)]
     #return list_text[6]
 
+# Сюда можно добавлять иконки разные, тоже будут случайно запускаться
 lambda_icons = ['''
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀#######⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -54,7 +57,52 @@ lambda_icons = ['''
                              \n''']
 
 
+
+# Вот эти названия тоже можно менять
+img_directory = '/home/adeline/Documents_my/img/wallpaper'
+hyprpaper_config = '/home/adeline/.config/hypr/hyprpaper.conf'
+
+
+
 #---------------- don't change please) --------------
+
+
+# Работа с hyprpaper и меняем обои
+
+def get_random_image(directory):
+    # Просматриваем фотографии которые есть и выбираем случайную фотку
+    image_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.gif','.PNG')): # Поддерживаемые форматы
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                image_files.append(filepath)
+
+    if not image_files:
+        return None
+
+    random_image = choice(image_files)
+    return random_image
+
+
+def create_hyprpaper_config(image_path, config_path):
+    config = f"""
+    preload = {image_path}
+    wallpaper = ,{image_path}
+    """
+    
+    with open(config_path, 'w') as f:
+        f.write(config)
+
+
+# img_directory = '/home/adeline/Documents_my/img/wallpaper'
+# hyprpaper_config = '/home/adeline/.config/hypr/hyprpaper.conf'
+
+random_img = get_random_image(img_directory)
+create_hyprpaper_config(random_img, hyprpaper_config)
+
+
+
 # Оранжевый цвет
 orange_color = "\033[38;5;214m"  # ANSI код для оранжевого цвета
 reset_color = "\033[0m"  # Сброс цвета
